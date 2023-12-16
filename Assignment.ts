a49287567d4bdd0fd3384af959772a22c88fe6f5
Sannaya Mughal <142016532+SannayaMughal@users.noreply.github.com>
2023-12-17 01:52:33 +0500

    Basic_Functions
    Basic_Functions
    Write a program that calculates the percentage.
     - Write a program that calculates the discount for a product based on its price. If the price is above $100, apply a 10% discount; otherwise, apply a 5% discount.
     - Create a program that determines the category of a user-provided age. If the age is between 0 and 12, print "Child." If it's between 13 and 19, print "Teenager." Otherwise, print "Adult."
     - Write a program that takes temperature and check it. If it is cold then suggest the user to wear warm clothes and so on according to the weather.
     - Write a program that checks if the given number is divisible by 3 or 5 or both or not divisible by anyone show output accordingly.
     - Write a program that checks if the given year is leap year or not.
     - Develop a program that determines the day of the week. Ask the user for a number (1-7) and use nested if statements to print the corresponding day's name.
     - Write a program that takes the number of units consumed by a user if it is greater than 100 then add 10% tax if greater than 200 then add 15% of tax so on up to if greater than 500 then add 25% of tax
    Where the tax amount will be calculated by the amount of bill.
     - Write a program that tells if the student is pass or fail based on the attendance greater than 75%, practical marks should greater than 50% and theory marks should be greater than 45% for passing in a semester.
     - Write a ts program that evaluates the grades of the students:
     * 80% or greater, grade A
     * 70% or greater, grade B
     * 60% or greater, grade C
     * 50% or greater, grade D
     * Less than 50% , grade F.

---
+++ Assignment.ts
@@ -0,0 +1,138 @@
+"use strict";
+function Numpercentage(obt:number, total:number) {
+
+    var Numpercentage = obt / total * 100;
+    console.log("percentage is ", Numpercentage);
+    return;
+}
+// Numpercentage(9, 10);
+
+
+function calculateDiscount(price:number) {
+    if (price > 100) {
+      return price * 0.1;
+    } else {
+      return price * 0.05;
+    }
+  }
+
+//   const discount = calculateDiscount(150);
+//   console.log(`Product price: 150 & Discount: ${discount}`);
+
+function getAgeCategory(age:number) 
+{
+    if (age >= 0 && age <= 12) {
+      return "Child";
+    } else if (age >= 13 && age <= 19) {
+      return "Teenager";
+    } else {
+      return "Adult";
+    }
+  }
+
+//   const category = getAgeCategory(15);
+//   console.log(`Your age category is: ${category}`);
+
+//Write a program that takes temperature and check it. If it is cold then suggest the user to wear warm clothes and so on according to the weather.
+function suggestClothing(temperature:number) {
+    if (temperature < 10) {
+        return "It's very cold! Wear a warm jacket, hat, gloves, and scarf.";
+    } else if (temperature < 15) {
+        return "It's cold! Wear a sweater or jacket and long pants.";
+    } else if (temperature < 20) {
+        return "It's cool! Wear a light jacket or long-sleeved shirt.";
+    } else if (temperature < 25) {
+        return "It's pleasant! Wear comfortable clothes like a t-shirt and jeans.";
+    } else if (temperature < 30) {
+        return "It's warm! Wear shorts or a light dress.";
+    } else {
+        return "It's very hot! Wear light, breathable clothing and stay hydrated.";
+    }
+}
+
+// Example usage
+const temperature = 18;
+// const clothingSuggestion = suggestClothing(temperature);
+// console.log(`The temperature is ${temperature}°C. You should wear ${clothingSuggestion}.`);
+
+
+function checkDivisibility(number:number)
+ {
+    if (number % 3 === 0 && number % 5 === 0) {
+        return `${number} is divisible by both 3 and 5.`;
+    } else if (number % 3 === 0) {
+        return `${number} is divisible by 3.`;
+    } else if (number % 5 === 0) {
+        return `${number} is divisible by 5.`;
+    } else {
+        return `${number} is not divisible by 3 or 5.`;
+    }
+}
+
+// Example usage
+// const number = 15;
+// const result = checkDivisibility(number);
+// console.log(result);
+
+function isLeapYear(year:number)
+ {
+    if (year % 4 === 0) {
+      // Check for divisibility by 100
+      if (year % 100 === 0) {
+        // Check for divisibility by 400 to avoid false positives
+        return year % 400 === 0;
+      } else {
+        return true; // Any year divisible by 4 but not by 100 is a leap year
+      }
+    } else {
+      return false; // Any year not divisible by 4 is not a leap year
+    }
+  }
+  // Example usage
+//   const year = 2024;
+//   const isLeap = isLeapYear(year);
+//   if (isLeap) {
+//     console.log(`${year} is a leap year.`);
+//   } else {
+//     console.log(`${year} is not a leap year.`);
+//   } 
+  /////////
+  
+  function calculateTax(units:number)
+   {
+    const baseCostPerUnit = 10;
+    var cost = units * baseCostPerUnit;
+
+    if (units > 100 && units <= 200) {
+        cost += cost * 0.1; // Add 10% tax if units > 100
+    } else if (units > 200 && units <= 500) {
+        cost += cost * 0.15; // Add 15% tax if units > 200
+    } else if (units > 500) {
+        cost += cost * 0.25; // Add 25% tax if units > 500
+    }
+
+    return cost;
+}
+
+// Example usage
+// const units = 350;
+// const totalCost = calculateTax(units);
+// console.log(`For ${units} units, the total cost is: $${totalCost.toFixed(2)}`);
+
+function checkPassFail(attendance:number, practicalMarks:number, theoryMarks:number)
+ {
+    if (attendance >= 75 && practicalMarks >= 50 && theoryMarks >= 45) {
+      return "Pass";
+    } else {
+      return "Fail";
+    }
+  }
+  // Example usage
+  const student1Attendance = 80;
+  const student1PracticalMarks = 65;
+  const student1TheoryMarks = 50;
+  const student2Attendance = 60;
+  const student2PracticalMarks = 40;
+  const student2TheoryMarks = 70;
+  console.log(`Student 1: ${checkPassFail(student1Attendance, student1PracticalMarks, student1TheoryMarks)}`);
+  console.log(`Student 2: ${checkPassFail(student2Attendance, student2PracticalMarks, student2TheoryMarks)}`);
